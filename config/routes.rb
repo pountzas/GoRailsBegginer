e3dc21536089d9a5b65f8bdc18fd8b5a64b86d6f
pountzas <nikos@pountzas.gr>
2021-05-15 00:22:24 +0300

    change a tags to ruby com link_to & _paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'about', to: 'about#index'
   
-  get '/', to: 'main#index'
+  root to: 'main#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
