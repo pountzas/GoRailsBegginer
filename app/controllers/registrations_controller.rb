2fafb44b6d4d62726fa66e1f74660ef1999936c5
pountzas <nikos@pountzas.gr>
2021-05-20 01:17:27 +0300

    add create on registrations controller with private param, add error mesages on new regist/views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,22 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    # render plain: params[:user]
+    # render plain: 'ok'
+    # @user = User.new(params[:user]) # not safe
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'welldone U managed to LOG'
+    else
+      render :new
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+  end
 end
